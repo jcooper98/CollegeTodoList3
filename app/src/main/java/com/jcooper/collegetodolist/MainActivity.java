426f20e00bb52e6b264a73a951975b20d5552d49
jackcooper <hibjack@icloud.com>
2019-04-25 18:04:35 -0500

    Stuck here

--- app/src/main/java/com/jcooper/collegetodolist/MainActivity.java
+++ app/src/main/java/com/jcooper/collegetodolist/MainActivity.java
@@ -1,19 +1,21 @@
 package com.jcooper.collegetodolist;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     Assignment assignmentList;
-
-
-
     ArrayAdapter<Assignment> assignmentAdapter;  // The custom array adapter for displaying the heart rates in the list view
-    ListView assignments;                       // The list view for the heart rates from the activity_main.xml file
+    ListView AssignmentsListView;                       // The list view for the heart rates from the activity_main.xml file
+
+
 
 
     @Override
@@ -21,8 +23,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AssignmentsListView = (ListView) findViewById(R.id.listViewAssignments);
+
 
         assignmentList = new Assignment("Test", false);
+        assignmentList.getTitle();
+
+        
+        assignmentAdapter.setDropDownViewResource(R.layout.todo_row);
+        //lvHeartRates.setAdapter(hrAdapter);
+
+
+
 
 
     }
